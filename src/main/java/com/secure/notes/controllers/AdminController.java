60c85096317661a9c279d074ba9933c4b54b8204
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-09-14 10:42:52 +0530

    enable method level security

--- src/main/java/com/secure/notes/controllers/AdminController.java
+++ src/main/java/com/secure/notes/controllers/AdminController.java
@@ -6,6 +6,7 @@ import com.secure.notes.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class AdminController {
     @Autowired
     UserService userService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getusers")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(),
